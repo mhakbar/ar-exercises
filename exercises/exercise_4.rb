d31eaf4b7f5684e7305687fb02aba82398b37514
mhakbar <habibakbar@live.com>
2022-03-10 20:43:01 -0800

    03 new stores created'

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,35 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+surrey = Store.new do |a|
+  a.name = "Surrey"
+  a.annual_revenue = 224000
+  a.womens_apparel = true
+  a.mens_apparel = false
+end
+
+surrey.save
+
+
+whistler = Store.new do |a|
+  a.name = "Whistler"
+  a.annual_revenue = 190000
+  a.womens_apparel = false
+  a.mens_apparel = true
+end
+
+whistler.save
+
+yaletown = Store.new do |a|
+  a.name = "Yaletown"
+  a.annual_revenue = 430000
+  a.womens_apparel = true
+  a.mens_apparel = true
+end
+
+yaletown.save
+
