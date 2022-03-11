0fbb40b19f0da564b0e7f6232e33597d894d2eb2
mhakbar <habibakbar@live.com>
2022-03-10 22:06:46 -0800

    completed exercise 5, added sum, average and count methods

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Total Annual revenue is: $#{Store.sum('annual_revenue')}"
+
+puts "Average annual revenue of each store is: $#{Store.average('annual_revenue')}"
+
+puts " The number of stores that earn more than 1 Million annually are: #{Store.where('annual_revenue > 1000000').count}"
+
+
+
+
