bf68c88146dd566c9e3547668d0e88e69c9686d6
mhakbar <habibakbar@live.com>
2022-03-10 20:11:08 -0800

    exercise 1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,35 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+#https://edgeguides.rubyonrails.org/active_record_basics.html   section: 5.1
+
+
+burnaby = Store.new do |a|
+  a.name = "Burnaby"
+  a.annual_revenue = 300000
+  a.mens_apparel = true
+  a.womens_apparel = true
+end
+
+burnaby.save
+
+richmond = Store.new do |a|
+  a.name = "Richmond"
+  a.annual_revenue = 1260000
+  a.mens_apparel = false
+  a.womens_apparel = true
+end
+
+richmond.save
+
+gastown = Store.new do |a|
+  a.name = "Gastown"
+  a.annual_revenue = 190000
+  a.mens_apparel = true
+  a.womens_apparel = false
+end
+
+gastown.save
+
+puts Store.count
+
